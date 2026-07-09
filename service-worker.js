1d7a197a0a6e06f45046198aca9b5be799212ee6
Marbin Alarcon <marbinalarcon9@gmail.com>
2026-07-09 07:05:20 -0500

    jum

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'cybercard-v2.01';
+const CACHE_NAME = 'cybercard-v2.02';
 const ASSETS = ['/', '/index.html', '/css/estilos.css', '/css/cartas.css', '/css/resonsive.css', '/img/personajes/black-clover', '/img/personajes/bleach', '/img/personajes/caballeros-del-zodiaco', '/img/personajes/demon-slayer', '/img/personajes/dragon-ball', '/img/personajes/fairy-tail', '/img/personajes/fullmetal-alchemist', '/img/personajes/hunter-x-hunter', '/img/personajes/naruto', '/img/personajes/one-piece', '/img/personajes/solo-leveling', '/img/personajes/spy-x-family', '/img/personajes/jujutsu-kaisen'];
 
 self.addEventListener('install', e => {
