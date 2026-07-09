647b7d8fe957e7d919aeba04a190decafd648168
Marbin Alarcon <marbinalarcon9@gmail.com>
2026-07-09 16:29:21 -0500

    ajustes responsive

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'cybercard-v2.03';
+const CACHE_NAME = 'cybercard-v2.04';
 const ASSETS = ['/', '/index.html', '/css/estilos.css', '/css/cartas.css', '/css/resonsive.css', '/img/personajes/black-clover/asta.jpeg', '/img/personajes/bleach/ichigo-kurosaki.jpeg', '/img/personajes/caballeros-zodiaco/acua.jpeg', '/img/personajes/demon-slayer/genya-shinazugawa.jpeg', '/img/personajes/naruto/naruto.jpeg', '/img/personajes/one-piece/luffy.jpeg', '/img/personajes/solo-leveling/belion.jpeg', '/img/personajes/jujutsu-kaisen/choso.jpeg'];
 
 self.addEventListener('install', e => {
