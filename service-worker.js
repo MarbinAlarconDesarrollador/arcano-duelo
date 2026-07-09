e0964dbe2e903efd0ffafa2103121378440976f4
Marbin Alarcon <marbinalarcon9@gmail.com>
2026-07-09 06:58:45 -0500

    personajes de jujutsu

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'cybercard-v2';
-const ASSETS = ['/', '/index.html', '/css/estilos.css', '/js/app.js', '/js/mazo.js', '/js/personajes.js', '/js/red.js', '/js/carta.js', '/img/personajes/dragon_cosmico.png', '/img/personajes/robot.png'];
+const CACHE_NAME = 'cybercard-v2.01';
+const ASSETS = ['/', '/index.html', '/css/estilos.css', '/css/cartas.css', '/css/resonsive.css', '/img/personajes/black-clover', '/img/personajes/bleach', '/img/personajes/caballeros-del-zodiaco', '/img/personajes/demon-slayer', '/img/personajes/dragon-ball', '/img/personajes/fairy-tail', '/img/personajes/fullmetal-alchemist', '/img/personajes/hunter-x-hunter', '/img/personajes/naruto', '/img/personajes/one-piece', '/img/personajes/solo-leveling', '/img/personajes/spy-x-family', '/img/personajes/jujutsu-kaisen'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
